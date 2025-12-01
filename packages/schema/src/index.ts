d20c8d5e15f2c0566f8e936f9801a5f17ce7959f
Segev Shmueli <cj@segevs.com>
2025-11-30 21:12:22 -0500

    feat(schema): export new property and credential schemas
    - Export property schema for external use
    - Export credential-related schemas (credential, oauth2Claims, oauth2Config, apiTokenConfig, authMethod)
    - Export variable schema (already in file)

--- packages/schema/src/index.ts
+++ packages/schema/src/index.ts
@@ -2,3 +2,12 @@ export { nodeSchema } from "./node";
 export { connectionSchema } from "./connection";
 export { workflowSchema } from "./workflow";
 export { edgeSchema } from "./edge";
+export { variableSchema } from "./variable";
+export { propertySchema } from "./property";
+export {
+  credentialSchema,
+  oauth2ClaimsSchema,
+  oauth2ConfigSchema,
+  apiTokenConfigSchema,
+  authMethodSchema,
+} from "./credentials";
